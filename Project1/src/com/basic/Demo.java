e30e2285aa6dbf843ef6d682c2b9aea16f91c08b
Dev1 <Dev1@gmail.com>
2023-10-10 11:37:51 +0530

    change proj1

---
+++ Project1/src/com/basic/Demo.java
@@ -0,0 +1,8 @@
+package com.basic;
+
+public class Demo {
+  public void display() {
+	  System.out.println("hello");
+}
+
+}
---
+++ Project1/src/com/basic/T1.java
@@ -0,0 +1,12 @@
+package com.basic;
+
+public class T1 {
+	public void test() {
+		System.out.println("test 2222222222222");
+	}
+
+
+public void m3(){
+	
+     }
+}
